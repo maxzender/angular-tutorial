70c8ec2769040d43913a884627e150670a7b6aca
Max Zender <mail@maxzender.de>
2017-10-18 17:35:13 +0200

    Display ArtistComponent using router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ArtistListComponent } from './artist-list/artist-list.component';
 import { ArtistService } from './artist.service';
 import { ArtistComponent } from './artist/artist.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ArtistListComponent },
+  { path: 'artists/:id', component: ArtistComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +19,8 @@ import { ArtistComponent } from './artist/artist.component';
     ArtistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ArtistService],
   bootstrap: [AppComponent]
